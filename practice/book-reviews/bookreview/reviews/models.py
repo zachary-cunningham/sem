640a956572fb72e0292ca02f7fc1efafdafca5ac
Zachary Cunningham <zach.b.cunningham@icloud.com>
2026-05-02 14:29:42 +0100

    create and register Review model

--- practice/book-reviews/bookreview/reviews/models.py
+++ practice/book-reviews/bookreview/reviews/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Review(models.Model):
 	# required
-	title = models.CharField()
-	author = models.CharField()
+	title = models.CharField(max_length=50)
+	author = models.CharField(max_length=50)
 	rating = models.IntegerField()
 	
 	body = models.TextField(blank=True) # allow blank
